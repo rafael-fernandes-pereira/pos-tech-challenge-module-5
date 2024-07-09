2cc47deb0435ee4dd5c80e13702918627bba64db
Rafael <rafael.ferper@gmail.com>
2024-07-08 22:21:52 -0300

    Products

---
+++ products/src/main/java/com/github/rafaelfernandes/products/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.github.rafaelfernandes.products.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
+public class ProductController {
+
+}
